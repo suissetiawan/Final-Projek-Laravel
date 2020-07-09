161b9e9a2d1237aa798dfc2aa4a86a90371e706d
RevDonz <aprilio842@gmail.com>
2020-07-09 08:25:12 +0700

    membuat QuestionController dan model

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::group(['middleware' => 'auth'], function() {
+	Route::resource('questions', 'QuestionController');
+	Route::get('/home', 'HomeController@index')->name('home');
+})
+
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
